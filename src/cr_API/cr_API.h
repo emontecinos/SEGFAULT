136924120f838dc4f8ccf02b1b9028ba689e432c
Eduardo Montecinos <efmontecinos@uc.cl>
2020-05-26 14:24:01 -0400

    struct

--- src/cr_API/cr_API.h
+++ src/cr_API/cr_API.h
@@ -13,28 +13,21 @@ typedef struct crFILE
 char* PATH;
 
 void cr_mount(char* diskname);
-
 void cr_bitmap(unsigned disk, bool hex);
-
 int cr_exists(unsigned disk, char* filename);
-
-void cr_ls(unsigned disk);
-
-crFILE* cr_open(unsigned disk, char* filename, char mode);
-
-int cr_read (crFILE* file_desc, void* buffer, int nbytes);
-
 int cr_write(crFILE* file_desc, void* buffer, int nbytes);
-
 int cr_close(crFILE* file_desc);
 
+
+void cr_ls(unsigned disk);
+crFILE* cr_open(unsigned disk, char* filename, char mode);
+int cr_read (crFILE* file_desc, void* buffer, int nbytes);
+
+
 int cr_rm(unsigned disk, char* filename);
-
 int cr_hardlink(unsigned disk, char* orig, char* dest);
-
 int cr_softlink(unsigned disk_orig, unsigned disk_dest, char* orig, char* dest);
 
 int cr_unload(unsigned disk, char* orig, char* dest);
-
 int cr_load(unsigned disk, char* orig);
 
