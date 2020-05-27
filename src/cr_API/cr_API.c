1b953091f0e79db85a0e3f5f11fb2b488c8f5bfd
Raimundo Eyzaguirre <reyzaguirre1@uc.cl>
2020-05-27 04:59:47 -0400

    1

--- src/cr_API/cr_API.c
+++ src/cr_API/cr_API.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <sys/types.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <fcntl.h>
 #include "cr_API.h"
 
@@ -32,7 +32,7 @@ void cr_bitmap(unsigned disk, bool hex){
             aux_bytes_leidos=fread(aux_buffer,1,1,file);
             for (int i =0; i < 8; i++){
                 aux_bit= aux_buffer[0]&1; // and entre el byte leido y 1; al shiftearlo voy bit por bit.
-                
+
                 if(aux_bit==1){ // aumento el contador
                     bloques_ocupados++;
                 }else {
@@ -83,7 +83,35 @@ int cr_rm(unsigned disk, char* filename){
     return 1;
 }
 int cr_hardlink(unsigned disk, char* orig, char* dest){
-    return 1;
+  unsigned char buffer[65536]; 
+  char aux_buffer[1];
+  size_t bytes_leidos=0;
+  unsigned int byte_lectura;
+  int aux_bytes_leidos=0;
+  int aux_bit;
+  int pos_buffer=0;
+  FILE* file = fopen(PATH,"rb");
+  byte_lectura=(disk-1)*536870912; // tamano particion
+  // lectura en bytes
+  fseek(file,byte_lectura,SEEK_SET); // puntero lectura desde posicion byte_lectura desde el inicio (SEEK_SET)
+  while(bytes_leidos < 8192){
+      aux_bytes_leidos=fread(aux_buffer,1,1,file);
+      for (int i =0; i < 8; i++){
+          aux_bit= aux_buffer[0]&1; // and entre el byte leido y 1; al shiftearlo voy bit por bit.
+
+          if(aux_bit==1){ // aumento el contador
+              bloques_ocupados++;
+          }else {
+              bloques_desocupados++;
+              }
+          buffer[pos_buffer]=aux_bit+'0';
+          aux_buffer[0]>>=1; //shift right en 1.
+          aux_bit++;
+          pos_buffer++;
+      }
+      bytes_leidos += 1;
+  }
+  return;
 }
 int cr_softlink(unsigned disk_orig, unsigned disk_dest, char* orig, char* dest){
     return 1;
